492621e96a35062398140cba48d76321b8b4a11d
dlvls <delvia.lana07@gmail.com>
2024-07-26 09:45:45 +0700

    final

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mini_project_2/resources/colors.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashPage extends StatefulWidget {
@@ -10,14 +13,34 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  int _filledDots = 0;
+  late Timer _timer;
+
   @override
   void initState() {
     super.initState();
+    _startTimer();
     _navigateToNextScreen();
   }
 
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_filledDots < 5) {
+          _filledDots++;
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   Future<void> _navigateToNextScreen() async {
-    await Future.delayed(const Duration(seconds: 2)); // Simulate a delay
+    await Future.delayed(const Duration(seconds: 5));
     final prefs = await SharedPreferences.getInstance();
     final bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
 
@@ -32,10 +55,32 @@ class _SplashPageState extends State<SplashPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset(
-          'assets/images/logo.png',
-          width: 200.0,
-          height: 200.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: 250.0,
+              height: 250.0,
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(5, (index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Container(
+                    width: 15,
+                    height: 15,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: index < _filledDots ? primaryColor : Colors.grey,
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ],
         ),
       ),
     );
