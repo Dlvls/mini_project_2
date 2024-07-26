64da09f4960dd648281e5e3bb48b319a6622abf7
dlvls <delvia.lana07@gmail.com>
2024-07-26 10:26:31 +0700

    final animation

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:mini_project_2/resources/colors.dart';
+import 'package:mini_project_2/resources/colors.dart'; // Make sure this file is correctly referenced
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashPage extends StatefulWidget {
@@ -12,35 +12,33 @@ class SplashPage extends StatefulWidget {
   _SplashPageState createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
-  int _filledDots = 0;
-  late Timer _timer;
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
-    _startTimer();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    )..repeat(reverse: true);
+    _animation = Tween<double>(begin: 0, end: 5).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.linear,
+    ));
     _navigateToNextScreen();
-  }
-
-  void _startTimer() {
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        if (_filledDots < 5) {
-          _filledDots++;
-        }
-      });
-    });
   }
 
   @override
   void dispose() {
-    _timer.cancel();
+    _controller.dispose();
     super.dispose();
   }
 
   Future<void> _navigateToNextScreen() async {
-    await Future.delayed(const Duration(seconds: 5));
+    await Future.delayed(const Duration(seconds: 5)); // Simulate a longer delay
     final prefs = await SharedPreferences.getInstance();
     final bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
 
@@ -64,21 +62,28 @@ class _SplashPageState extends State<SplashPage> {
               height: 250.0,
             ),
             const SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(5, (index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                  child: Container(
-                    width: 15,
-                    height: 15,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: index < _filledDots ? primaryColor : Colors.grey,
-                    ),
-                  ),
+            AnimatedBuilder(
+              animation: _animation,
+              builder: (context, child) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(5, (index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: index < _animation.value
+                              ? primaryColor
+                              : Colors.grey,
+                        ),
+                      ),
+                    );
+                  }),
                 );
-              }),
+              },
             ),
           ],
         ),
